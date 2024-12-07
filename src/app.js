7fca35a88f37950c46b4b56b06c375936f75fbf3
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-12-07 00:24:12 +0000

    excuse generator done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const choosenWho = chooseItem(who);
+
+  let action = ["ate", "peed", "crushed", "broke"];
+  const choosenAction = chooseItem(action);
+
+  let what = ["my homework", "my phone", "the car"];
+  const choosenWhat = chooseItem(what);
+
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const choosenWhen = chooseItem(when);
+
+  const newExcuse = `${choosenWho} ${choosenAction} ${choosenWhat} ${choosenWhen}`;
+
+  const p = document.querySelector("#excuse");
+  p.textContent = newExcuse;
+
+  function chooseItem(array) {
+    let randomItem = Math.floor(Math.random() * array.length);
+    return array[randomItem];
+  }
 };
